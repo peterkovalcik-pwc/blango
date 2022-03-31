7d61953ac26a07bf2038fbb1ca302dff5e7a937e
Peter Kovalčík <peter.kovalcik@gmail.com>
2022-03-31 12:49:41 +0000

    Update urls

--- blango/urls.py
+++ blango/urls.py
@@ -20,5 +20,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
